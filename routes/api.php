f445a5a838f5a6c731bbc2cf4a2644f512783264
nkiesler <ninna.karapetyan@gmail.com>
2020-02-22 23:43:54 +0400

    signup

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('signup, 'ApiController@signup');
+Route::post('signup', 'ApiController@signup');
