72ce75b7d96aec1ddef7f09e73dd638fc8bd99eb
nkiesler <ninna.karapetyan@gmail.com>
2020-02-22 22:44:23 +0400

    configured database

--- config/database.php
+++ config/database.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Str;
 
 return [
 
+    $url = parse_url(getenv("DATABASE_URL"));
+    $host = $url["host"]??null;
+    $username = $url["user"]??null;
+    $password = $url["pass"]??null;
+    $database = substr($url["path"], 1)??null;
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -15,7 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_prod'),
 
     /*
     |--------------------------------------------------------------------------
@@ -32,6 +37,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+    
 
     'connections' => [
 
@@ -76,6 +82,17 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],
+
+        'pgsql_production' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf-8',
+            'prefix' => '',
+            'schema' => 'public',
         ],
 
         'sqlsrv' => [
