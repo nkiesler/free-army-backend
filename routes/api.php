cb270ff0ac9a63e4f6de391a749dd1a5716aa08a
nkiesler <ninna@tutanota.com>
2020-02-23 20:18:16 +0400

    cors fix

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
