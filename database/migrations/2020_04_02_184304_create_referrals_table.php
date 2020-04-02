c10db14e7fd06772f829ae25be2ff82068f8b8d4
Nina <ninna.karapetyan@gmail.com>
2020-04-02 23:11:12 +0400

    referrals table

---
+++ database/migrations/2020_04_02_184304_create_referrals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReferralsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('referrals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('referrer_id')->nullable();
+            $table->foreign('referrer_id')->references('id')->on('users');
+            $table->string('user_email')->unique();
+            $table->boolean('registered')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('referrals');
+    }
+}
